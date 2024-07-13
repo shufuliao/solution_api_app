dc01f5b6c31d5807198f6065241b994ead6ddbe5
Shufu Liao <liaovincent7801@gmail.com>
2024-07-13 10:33:08 +0800

    feat api/orders create 功能

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,5 +1,19 @@
 class Api::OrdersController < ApplicationController
   def create
-    render json: { message: "Order created" }, status: :created
+    order = order_params
+
+    order_service = OrderService.new(order)
+
+    order_service.transform_currency if order_service.valid?
+
+    render json: order, status: :ok
+  rescue => e
+    render json: { error: e.message }, status: :bad_request
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:id, :name, :price, :currency, address: [:city, :district, :street])
   end
 end
